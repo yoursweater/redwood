12024fea0f8222568e192855a6b5dc257ab769cb
yoursweater <daneversen@gmail.com>
2022-08-17 12:41:15 -0400

    tutorial

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -1,3 +1,5 @@
+import { Link, routes } from '@redwoodjs/router'
+
 export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
@@ -23,7 +25,11 @@ export const Success = ({ articles }) => {
       {articles.map((article) => (
         <article key={article.id}>
           <header>
-            <h2>{article.title}</h2>
+            <h2>
+              <Link to={routes.article({ id: article.id })}>
+                {article.title}
+              </Link>
+            </h2>
           </header>
           <p>{article.body}</p>
           <div>Posted at: {article.createdAt}</div>
